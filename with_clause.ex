51fe5b3670b9418ce0e0ef87b9145c54e8c20571
z <x@y.com>
2018-05-31 17:56:25 +0200

    Finished initial intro to with-else clause

--- with_clause.ex
+++ with_clause.ex
@@ -27,6 +27,51 @@ IO.inspect with {:ok, width} <- Map.fetch(opts, :width),
 
 IO.puts """
 
+
+defmodule Address do
+  defstruct street: nil, city: "New York"
+end
+
+defmodule Organization do
+  defstruct name: "IBM", address: nil
+
+  def create_address( params ) do
+    %Address{ street: params[:street] }
+  end
+
+  def create_organization( params ) do
+    %Organization{ name: params[:name], address: params[:address] }
+  end
+end
+
+defmodule Main do
+  def main(params) do
+
+    address = %Address{}
+    organization = %Organization{}
+    IO.write "Address - "
+    IO.inspect address
+    IO.write "Organization - "
+    IO.inspect organization
+
+    result = with address <-
+                    Organization.create_address(params),
+                  {:ok, organization} <-
+                    Organization.create_organization( %{ name: "Apple",  address: address } )
+             do
+                {:ok, %{organization | address: address}}
+             else
+               %{"error" => reason} -> {:error, reason}
+               error -> error
+             end
+    IO.write "Result - "
+    IO.inspect result
+
+  end
+end
+
+params = %{street: '1 Broadway'}
+result = Main.main( params )
 """
 
 defmodule Address do
@@ -47,20 +92,29 @@ end
 
 defmodule Main do
   def main(params) do
+
+    address = %Address{}
+    organization = %Organization{}
+    IO.write "Address - "
+    IO.inspect address
+    IO.write "Organization - "
+    IO.inspect organization
+
     result = with address <-
                     Organization.create_address(params),
                   {:ok, organization} <-
-                    Organization.create_organization( %{ address: address }  )
+                    Organization.create_organization( %{ name: "Apple",  address: address } )
              do
                 {:ok, %{organization | address: address}}
              else
                %{"error" => reason} -> {:error, reason}
                error -> error
              end
+    IO.write "Result - "
     IO.inspect result
+
   end
 end
 
 params = %{street: '1 Broadway'}
-
-Main.main( params )
+result = Main.main( params )
